fcccf9628f46ac9f1c512be037a99a1e1be6ba3b
Luiz Otavio <luizomf@gmail.com>
2021-02-16 12:06:10 -0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState, useRef } from 'react';
 import './App.css';
 
-const Post = ({ post }) => {
+const Post = ({ post, handleClick }) => {
   console.log('Filho renderizou');
   return (
     <div key={post.id} className="post">
-      <h1>{post.title}</h1>
+      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
       <p>{post.body}</p>
     </div>
   );
@@ -18,27 +20,43 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
+  handleClick: P.func,
 };
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(0);
 
   console.log('Pai renderizou!');
 
   // Component did mount
   useEffect(() => {
-    setTimeout(function () {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r));
-    }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
   }, []);
+
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
+
+  useEffect(() => {
+    contador.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
 
   return (
     <div className="App">
+      <h6>Renderizou: {contador.current}x</h6>
       <p>
         <input
+          ref={input}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -48,7 +66,7 @@ function App() {
         return (
           posts.length > 0 &&
           posts.map((post) => {
-            return <Post key={post.id} post={post} />;
+            return <Post key={post.id} post={post} handleClick={handleClick} />;
           })
         );
       }, [posts])}
