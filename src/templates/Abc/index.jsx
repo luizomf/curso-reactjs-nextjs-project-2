512df3ce49f7c709d4d9400c448c010e532b1a65
Luiz Otavio <luizomf@gmail.com>
2021-02-17 21:52:51 -0300

    useHistory e useLocation

--- src/templates/Abc/index.jsx
+++ src/templates/Abc/index.jsx
@@ -1,7 +1,16 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
 
 export const Abc = () => {
   const { slug, id } = useParams();
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(function () {
+      history.push('/');
+    }, 5000);
+  }, [history]);
 
   return (
     <div>
--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -3,8 +3,18 @@ import './styles.css';
 import { PostsProvider } from '../../contexts/PostsProvider';
 import { Posts } from '../../components/Posts';
 import { CounterProvider } from '../../contexts/CounterProvider';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export function App() {
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(function () {
+      history.push('/abc');
+    }, 5000);
+  }, [history]);
+
   return (
     <CounterProvider>
       <PostsProvider>
